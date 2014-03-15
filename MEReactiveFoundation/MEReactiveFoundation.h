d464e0f0c53cd92ffe334650a11f9c19b8982ebd
William Towe <willbur1984@gmail.com>
2014-03-15 18:19:20 -0400

    Initial Commit

---
+++ MEReactiveFoundation/MEReactiveFoundation.h
@@ -0,0 +1,13 @@
+//
+//  MEReactiveFoundation.h
+//  MEReactiveFoundation
+//
+//  Created by William Towe on 3/15/14.
+//  Copyright (c) 2014 Maestro, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MEReactiveFoundation : NSObject
+
+@end
---
+++ MEReactiveFoundationTests/MEReactiveFoundationTests.m
@@ -0,0 +1,34 @@
+//
+//  MEReactiveFoundationTests.m
+//  MEReactiveFoundationTests
+//
+//  Created by William Towe on 3/15/14.
+//  Copyright (c) 2014 Maestro, LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MEReactiveFoundationTests : XCTestCase
+
+@end
+
+@implementation MEReactiveFoundationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
